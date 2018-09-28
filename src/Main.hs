d8519258138f2da9246e90725f676365ca101033
Karl Smeltzer <karl@galois.com>
2018-09-28 16:09:06 -0700

    use file-embed instead of copy/paste

--- src/Main.hs
+++ src/Main.hs
@@ -5,35 +5,16 @@ module Main where
 import System.FilePath ( (</>) )
 
 import Abide.ABI.SystemV as SV
-import Abide.Compute
 import Abide.Generate
 import Abide.Parse
-import Abide.Types
-import Abide.Types.ABI.SystemV
-import Abide.Types.Arch.X86_64
 
 main :: IO ()
-main = generatePPC
+main = generatePPC >> generateX64
 
--- An example of generating the X86_64 finite state transducer.  We need a
--- better story for generating these and then parsing them for use without
--- having to repeat the work.
-generateX86 :: IO ()
-generateX86 = do
-  generateFiles ("/home/karl/code/abide/test/" </> SV.x86fp) (mkFSTGen SV.x86_64Typemap)
-  efst <- x86_64FSTFromFile "/home/karl/code/abide/test/x86_64.fst.txt"
-  case efst of
-    Left err -> print err
-    Right fst -> print $ transduce fst fakeX64Input
+generateX64 :: IO ()
+generateX64 =
+  generateFiles "fst-files/x86_64" (mkFSTGen SV.x86_64Typemap)
 
 generatePPC :: IO ()
-generatePPC = do
-  generateFiles "/home/karl/code/abide/test/ppc" (mkFSTGen SV.ppcTypemap)
-  efst <- ppcFSTFromFile "/home/karl/code/abide/test/ppc.fst.txt"
-  case efst of
-    Left err -> print err
-    Right fst -> print $ transduce fst fakePPCInput
-  
-fakeX64Input = [INTEGER, INTEGER, SSE]
-
-fakePPCInput = [PPCGP, PPCGP, PPCFLOAT]
+generatePPC =
+  generateFiles "fst-files/ppc" (mkFSTGen SV.ppcTypemap)
